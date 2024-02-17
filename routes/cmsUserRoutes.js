f9fc7855138043003d027296e8c6a89361922640
SanjnaPanwar <sanjnapanwar7697@gmail.com>
2024-02-17 15:13:20 +0530

    service call changed

--- routes/cmsUserRoutes.js
+++ routes/cmsUserRoutes.js
@@ -1,7 +1,7 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const userService = require('../services/cmsUserService');
+const cmsUserService = require('../services/cmsUserService');
 
 router.post('/createNewUser', async (req, res) => {
   const { emailid, password, organisation, name, usertype } = req.body;
@@ -11,7 +11,7 @@ router.post('/createNewUser', async (req, res) => {
       return res.status(400).json({ message: 'Incomplete details' });
     }
 
-    await userService.createUser(emailid, password, organisation, name, usertype);
+    await cmsUserService.createUser(emailid, password, organisation, name, usertype);
     res.status(200).json({ message: 'User created' });
   } catch (error) {
     console.error(error);
@@ -24,7 +24,7 @@ router.post('/updatepassword', async (req, res) => {
   const { emailid, newpassword } = req.body;
 
   try {
-    const message = await userService.updatePassword(emailid, newpassword);
+    const message = await cmsUserService.updatePassword(emailid, newpassword);
     res.status(200).json({ message });
   } catch (error) {
     console.error(error);
@@ -40,7 +40,7 @@ router.delete('/deletecmsuser', async (req, res) => {
       return res.status(400).send('Please enter emailid');
     }
 
-    await userService.deleteCmsUser({ where: { emailid } });
+    await cmsUserService.deleteCmsUser({ where: { emailid } });
     res.status(200).send('CMS user account and related campaign entries deleted');
   } catch (error) {
     console.error('Error deleting user:', error);
@@ -52,7 +52,7 @@ router.post('/editUserDetails', async (req, res) => {
   const { name, password, usertype, oldemailid, newemailid } = req.body;
 
   try {
-    const message = await userService.editUserDetails(name, password, usertype, oldemailid, newemailid);
+    const message = await cmsUserService.editUserDetails(name, password, usertype, oldemailid, newemailid);
     res.status(200).json({ message });
   } catch (error) {
     console.error(error);
