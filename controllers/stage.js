79cf57db442ea951de75016415829d3520ab79b6
Mahendra <94590823+mahendra-shah@users.noreply.github.com>
2024-03-31 14:52:06 +0530

    fix

--- controllers/stage.js
+++ controllers/stage.js
@@ -235,7 +235,7 @@ module.exports = {
         /* #swagger.tags = ['Stage/Level']
            #swagger.summary = ' - get all signed urls for campaign with scantype'
            #swagger.parameters['campaign_id'] = {in: 'path', required: true, type: 'integer'}
-           #swagger.parameters['scantype'] = {in: 'path', required: true, type: 'integer', enum: ['qr', 'image']}           
+           #swagger.parameters['scantype'] = {in: 'path', required: true, type: 'string', enum: ['qr', 'image']}           
         */
         try {
             const { campaign_id, scantype } = req.params;
