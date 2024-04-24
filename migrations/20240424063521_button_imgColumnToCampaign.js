0d9e2daa2139b8a094ae979e5bced5a1aa5018d4
Mahendra <94590823+mahendra-shah@users.noreply.github.com>
2024-04-24 12:32:52 +0530

    fix migration

--- migrations/20240424063521_button_imgColumnToCampaign.js
+++ migrations/20240424063521_button_imgColumnToCampaign.js
@@ -1,5 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.alterTable('campaign_config', function (table) {
+      table.string('image').nullable().alter();
       table.string('button_img');
   });
 };
@@ -7,6 +8,7 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   // Revert the changes made in the 'up' function
   return knex.schema.alterTable('campaign_config', function (table) {
-    table.string('button_img');
+    table.string('image').notNullable().alter();
+    table.dropColumn('button_img');
   });
 };
